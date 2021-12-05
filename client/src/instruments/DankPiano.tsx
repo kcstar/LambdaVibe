e01ac98e3a7923f021469d0f070c7260acb29d1b
Kyle Chen <kylec641@gmail.com>
2021-12-05 03:30:01 -0800

    Derived violin implementation from flute implementation

--- client/src/instruments/DankPiano.tsx
+++ client/src/instruments/DankPiano.tsx
@@ -2,105 +2,49 @@
 import * as Tone from 'tone';
 import classNames from 'classnames';
 import { List, Range } from 'immutable';
-import React from 'react';
-
+import React, { useState } from 'react';
+import violin from '../img/665px-Violin-with-bow.svg';
 // project imports
-import { Instrument, InstrumentProps } from '../Instruments';
+import { Instrument } from '../Instruments';
 
 /** ------------------------------------------------------------------------ **
  * Contains implementation of components for Piano.
  ** ------------------------------------------------------------------------ */
-
-interface DankPianoKeyProps {
-  note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
-  duration?: string;
-  synth?: Tone.Synth; // Contains library code for making sound
-  minor?: boolean; // True if minor key, false if major key
+interface FluteButtonProps {
+  note: string;
   octave: number;
-  index: number; // octave + index together give a location for the piano key
 }
 
-export function DankPianoKey({
-  note,
-  synth,
-  minor,
-  index,
-}: DankPianoKeyProps): JSX.Element {
-  /**
-   * This React component corresponds to either a major or minor key in the piano.
-   * See `PianoKeyWithoutJSX` for the React component without JSX.
-   */
-  return (
-    // Observations:
-    // 1. The JSX refers to the HTML-looking syntax within TypeScript.
-    // 2. The JSX will be **transpiled** into the corresponding `React.createElement` library call.
-    // 3. The curly braces `{` and `}` should remind you of string interpolation.
-    <div
-      onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
-      onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
-      className={classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor, // minor keys are black
-        'black bg-white h4': !minor, // major keys are white
-      })}
-      style={{
-        // CSS
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
-      }}
-    ></div>
-  );
-}
+const sampler = new Tone.Sampler({
+  urls: {
+    A3: 'A3.mp3',
+    A4: 'A4.mp3',
+    A5: 'A5.mp3',
+    C3: 'C3.mp3',
+    C5: 'C5.mp3',
+    C6: 'C6.mp3',
+    E3: 'E3.mp3',
+    E4: 'E4.mp3',
+    E5: 'E5.mp3',
+  },
+  baseUrl: 'http://localhost:3000/samples/violin/',
+}).toDestination();
 
-// eslint-disable-next-line
-function DankPianoKeyWithoutJSX({
-  note,
-  synth,
-  minor,
-  index,
-}: DankPianoKeyProps): JSX.Element {
-  /**
-   * This React component for pedagogical purposes.
-   * See `PianoKey` for the React component with JSX (JavaScript XML).
-   */
-  return React.createElement(
-    'div',
-    {
-      onMouseDown: () => synth?.triggerAttack(`${note}`),
-      onMouseUp: () => synth?.triggerRelease('+0.25'),
-      className: classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor,
-        'black bg-white h4': !minor,
-      }),
-      style: {
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
-      },
-    },
-    [],
-  );
-}
-
-function DankPianoType({ title, onClick, active }: any): JSX.Element {
+function FluteButton({ note, octave }: FluteButtonProps): JSX.Element {
   return (
     <div
-      onClick={onClick}
-      className={classNames('dim pointer ph2 pv1 ba mr2 br1 fw7 bw1', {
-        'b--black black': active,
-        'gray b--light-gray': !active,
-      })}
-    >
-      {title}
+      className='f6 link dim br-pill ba bw2 ph3 pv2 mb2 dib black'
+      onClick={() => {
+        sampler.triggerAttackRelease(`${note}${octave}`, '4n');
+      }}>
+      {note}
     </div>
   );
 }
 
-function DankPiano({ synth, setSynth }: InstrumentProps): JSX.Element {
+function Flute(): JSX.Element {
+
+  const [octave, setOctave] = useState(6);
   const keys = List([
     { note: 'C', idx: 0 },
     { note: 'Db', idx: 0.5 },
@@ -116,61 +60,34 @@ function DankPiano({ synth, setSynth }: InstrumentProps): JSX.Element {
     { note: 'B', idx: 6 },
   ]);
 
-  const setOscillator = (newType: Tone.ToneOscillatorType) => {
-    setSynth(oldSynth => {
-      oldSynth.disconnect();
-
-      return new Tone.Synth({
-        oscillator: { type: newType } as Tone.OmniOscillatorOptions,
-      }).toDestination();
-    });
-  };
-
-  const oscillators: List<OscillatorType> = List([
-    'sine',
-    'sawtooth',
-    'square',
-    'triangle',
-    'fmsine',
-    'fmsawtooth',
-    'fmtriangle',
-    'amsine',
-    'amsawtooth',
-    'amtriangle',
-  ]) as List<OscillatorType>;
-
   return (
-    <div className="pv4">
-      <div className="relative dib h4 w-100 ml4">
-        {Range(2, 7).map(octave =>
-          keys.map(key => {
-            const isMinor = key.note.indexOf('b') !== -1;
-            const note = `${key.note}${octave}`;
-            return (
-              <DankPianoKey
-                key={note} //react key
-                note={note}
-                synth={synth}
-                minor={isMinor}
-                octave={octave}
-                index={(octave - 2) * 7 + key.idx}
-              />
-            );
-          }),
-        )}
+    <div className='pv4'>
+      <div className='fl w-50 pa2'>
+        <img src={violin} alt='Violin' />
       </div>
-      <div className={'pl4 pt4 flex'}>
-        {oscillators.map(o => (
-          <DankPianoType
-            key={o}
-            title={o}
-            onClick={() => setOscillator(o)}
-            active={synth?.oscillator.type === o}
-          />
-        ))}
+      <div className='fl w-100 pa2' style={{marginLeft: '2rem'}}> 
+        <label className="f6 b db mb2">
+          Current Octave: 
+          <select className="input-reset ba b--black-20 pa2 mb2 db w-10"value={octave} onChange={e => setOctave(parseInt(e.target.value))}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+          </select>
+        </label>
       </div>
+      <div className='relative dib h4 w-100 ml4'>
+        {keys.map(key => {
+          const note = `${key.note}`;
+          return  (<FluteButton note={note} octave={octave}/>) 
+        })}
+      </div>
+      
     </div>
   );
 }
 
-export const DankPianoInstrument = new Instrument('DankPiano', DankPiano);
+export const FluteInstrument = new Instrument('Violin', Flute);
