4b836e7b43f9f9556aacdd6ad24fa437f438c721
ksy415 <sykevin415@gmail.com>
2021-12-07 09:36:34 -0800

    fix bug where violin was using a nonexistent mp3 sample

--- client/src/instruments/kcstar.tsx
+++ client/src/instruments/kcstar.tsx
@@ -16,12 +16,13 @@ const sampler = new Tone.Sampler({
     A3: 'A3.mp3',
     A4: 'A4.mp3',
     A5: 'A5.mp3',
-    C3: 'C3.mp3',
-    C5: 'C5.mp3',
+    C4: 'C4.mp3',
     C6: 'C6.mp3',
-    E3: 'E3.mp3',
     E4: 'E4.mp3',
     E5: 'E5.mp3',
+    G3: 'G3.mp3',
+    G4: 'G4.mp3',
+    G5: 'G5.mp3'
   },
   baseUrl: 'http://localhost:3000/samples/violin/',
 }).toDestination();
